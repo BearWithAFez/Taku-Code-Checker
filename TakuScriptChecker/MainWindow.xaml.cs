b6c072505e47be913d0832786b8be6880c8d550d
Dwight VdV <dfr34kz@gmail.com>
2017-12-13 22:23:56 +0100

    First push

--- TakuScriptChecker/MainWindow.xaml.cs
+++ TakuScriptChecker/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace TakuScriptChecker
+namespace TakuCodeChecker
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
--- TakuScriptChecker/Properties/Resources.Designer.cs
+++ TakuScriptChecker/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace TakuScriptChecker.Properties
+namespace TakuCodeChecker.Properties
 {
 
 
