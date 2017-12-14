5cd283c6a906507143eeb9d1f6b62019f84a25ea
Dwight VdV <dfr34kz@gmail.com>
2017-12-14 13:41:22 +0100

    String side Complete

---
+++ TakuScriptChecker/Data.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+
+namespace TakuCodeChecker {
+    public class Data {
+        public string Name;
+        public string Author;
+        private List<Diff> _Diff = new List<Diff>();
+
+        public List<Diff> Diff {
+            get { return _Diff; }
+            set { _Diff = value; }
+        }
+    }
+
+    public class Diff {
+        public string Name;
+        private List<Puzzle> _Puzzle = new List<Puzzle>();
+
+		public List<Puzzle> Puzzle {
+			get { return _Puzzle; }
+			set { _Puzzle = value; }
+        }
+    }
+
+    public class Puzzle {
+        public int Id;
+        public string Code;
+    }
+}
--- TakuScriptChecker/MainWindow.xaml.cs
+++ TakuScriptChecker/MainWindow.xaml.cs
@@ -1,28 +1,124 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.Win32;
+using Newtonsoft.Json;
+using System;
+using System.IO;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
-namespace TakuCodeChecker
-{
+namespace TakuCodeChecker {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
-    {
-        public MainWindow()
-        {
+    public partial class MainWindow : Window {
+        // Vars
+        private readonly char[] ALLOWEDCHARS = { '1', '0', 'e' };
+        private OpenFileDialog ofd = new OpenFileDialog();
+
+        public MainWindow() {
+            // General init
             InitializeComponent();
+            tbOutput.AppendText("Welcome, please give me some data.\n");
+            tbOutput.ScrollToEnd();
+
+            // Init OpenFileDialog
+            ofd.InitialDirectory = "c:\\";
+            ofd.Filter = "json files (*.json)|*.json";
+            ofd.RestoreDirectory = true;
+        }
+
+        private void BtnChooseFile_Click(object sender, RoutedEventArgs e) {
+            // File loading
+            if (ofd.ShowDialog() == true) {
+                tbOutput.AppendText("File " + Path.GetFileNameWithoutExtension(ofd.FileName) + " loaded.\n");
+                tbOutput.ScrollToEnd();
+                btnFileCheck.IsEnabled = true;
+                tbFile.Text = Path.GetFileName(ofd.FileName);
+            } else {
+                ofd.FileName = "";
+                tbOutput.AppendText("No file selected.\n");
+                tbOutput.ScrollToEnd();
+                btnFileCheck.IsEnabled = false;
+                tbFile.Text = "";
+            }
+        }
+
+        private void BtnFileCheck_Click(object sender, RoutedEventArgs e) {
+            Stream myStream;
+            Data input = new Data();
+            try {
+                if ((myStream = ofd.OpenFile()) != null) using (StreamReader r = new StreamReader(myStream)) input = JsonConvert.DeserializeObject<Data>(r.ReadToEnd());
+            } catch (Exception ex) {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+            Console.WriteLine(input);
+        }
+
+        private void BtnStringCheck_Click(object sender, RoutedEventArgs e) {
+            string input = tbString.Text.Trim();
+
+            // Empty input
+            if (input == "") {
+                // No valid input
+                tbOutput.AppendText("There's nothing there to validate...\n");
+                tbOutput.AppendText("---------------------------------------\n");
+                tbOutput.ScrollToEnd();
+                return;
+            }
+
+            // Check the string
+            int errors = CheckString(input);
+
+            // Show errors
+            if ((errors & 0b10) == 0b10) tbOutput.AppendText("Invalid Char in input.\n");
+            if ((errors & 0b01) == 0b01) tbOutput.AppendText("Invalid Length of input.\n");
+            if (errors == 0) tbOutput.AppendText("Valid input!\n");
+            tbOutput.AppendText("---------------------------------------\n");
+            tbOutput.ScrollToEnd();
+        }
+
+        private int CheckString(string toCheck) {
+            // Vars
+            int errs = 0b00;
+
+            // Check length
+            if (!IsPerfectSquare(toCheck.Length)) errs |= 0b01;
+
+            // Check Chars
+            if (!StringOnlyContainsChars(toCheck)) errs |= 0b10;
+
+            // No issue
+            return errs;
+        }
+
+        private bool IsPerfectSquare(int number) {
+            // This only for puzzles up to 14x14. So unless you have giant puzzles, this will suffice.
+
+            // Odd
+            if (number % 2 == 1) return false;
+
+            switch (number) {
+                case 4:     // 2x2
+                case 16:    // 4X4
+                case 36:    // 6x6
+                case 64:    // 8x8
+                case 100:   // 10x10
+                case 144:   // 12x12
+                case 196:   // 14x14
+                    return true;
+                default:
+                    return false;
+            }
+        }
+
+        private bool StringOnlyContainsChars(string toCheck) {
+            // Loop over string
+            foreach (char c in toCheck) {
+                bool matched = false;
+                foreach (char k in ALLOWEDCHARS) if (c == k) matched = true;
+                if (!matched) return false;
+            }
+
+            // No errors found
+            return true;
         }
     }
 }
